65c3d37b4df4bbec54d371dc3ae61447757dff87
Dom Jackson <dom.jackson@bbc.co.uk>
2026-01-04 11:28:23 +0000

    Update pointers example

--- pointers.c
+++ pointers.c
@@ -4,12 +4,12 @@ int main(void)
 {
 
     int i;
-    int *p;
+    int *p; // Pointer
 
-    p = &i;
+    p = &i; // Assign address of i to p
 
     i = 10;  // i is now 10
-    *p = 20; // the thing p points to (namely i!) is now 20!!
+    *p = 20; // the thing p points to (i) is now 20
 
     printf("i is %d\n", i);   // prints "20"
     printf("i is %d\n", *p);  // "20"! dereference-p is the same as i!
