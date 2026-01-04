ff8b966f45b6f495a900a4654324c5825a11e801
domjacks <me@domjackson.xyz>
2026-01-04 17:39:33 +0000

    Finish off pointers

--- pointers.c
+++ pointers.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 
+void increment(int *n) {
+    *n = *n + 1;
+}
+
 int main(void)
 {
-
     int i;
     int *p; // Pointer
+    int *np = NULL;
 
+    printf("Assigning p to address of i\n");
     p = &i; // Assign address of i to p
 
+    printf("Assigning i to 10\n");
     i = 10;  // i is now 10
+
+    printf("Dereferencing p and assigning it to 20\n");
     *p = 20; // the thing p points to (i) is now 20
 
     printf("i is %d\n", i);   // prints "20"
-    printf("i is %d\n", *p);  // "20"! dereference-p is the same as i!
+    printf("*p is %d\n", *p);  // "20"! dereference-p is the same as i!
+
+    printf("np is %p\n", np); 
+ 
+    printf("Incrementing pointer p by 1\n");
+    increment(p);
+
+    printf("i is %d\n", i);  // "20"! dereference-p is the same as i!
+
+
 }
 
